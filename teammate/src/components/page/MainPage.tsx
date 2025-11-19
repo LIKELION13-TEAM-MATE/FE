3877536589b94470edf6144d737e311fe19a4d0b
unknown <sooolchy@gmail.com>
2025-11-19 17:33:01 +0900

    앱 기본 틀 제작

---
+++ teammate/src/components/page/MainPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function MainPage() {
+  return (
+    <MainWrapper>
+      MainPage
+    </MainWrapper>
+  );
+}
+
+const MainWrapper = styled.div`
+  /* 스타일은 여기 */
+`;
+
+export default MainPage;
