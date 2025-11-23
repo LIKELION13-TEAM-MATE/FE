2afac01cd038b2e0349ab5a300cb89413addaa3d
2godong <sora050821@gmail.com>
2025-11-24 03:28:14 +0900

    역할 페이지 개발

--- teammate/src/App.tsx
+++ teammate/src/App.tsx
@@ -6,6 +6,7 @@ import { Route, Routes, Outlet } from 'react-router-dom';
 
 // 페이지 이 밑으로 쭉 정리해주세요.
 import MainPage from './components/page/MainPage';
+import RolePage from './components/page/RolePage';
 
 //페이지 외 요소들은 이 밑으로 정리해주세요.
 
@@ -28,6 +29,7 @@ function App() {
         <Route element={<MainLayout />}>
         
           <Route path="/" element={<MainPage />}></Route>
+          <Route path="/RolePage" element={<RolePage />}></Route>
       
         </Route>
       </Routes>
